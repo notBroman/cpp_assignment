4d6a0b5f91b292f3167f65c777b9762a56235430
Roman Berger <romanberger001@gmail.com>
2022-12-02 14:30:29 +0000

    screenshots

---
+++ build/CMakeFiles/a.out.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/notbroman/Documents/uni/Y3/EE3093/assignment_2/src/main.cpp" "CMakeFiles/a.out.dir/src/main.cpp.o" "gcc" "CMakeFiles/a.out.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
