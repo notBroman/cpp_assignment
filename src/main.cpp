0b241d75a35792a1eae875a98c1369d0e5a03a82
Roman Berger <romanberger001@gmail.com>
2022-12-04 13:18:27 +0000

    some testing

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 int main(int argv, char* argc[]){
 
     FlightTimetableEntry a;
+    a.checkEntryVals(Aberdeen, London, BA, 590, 6,0);
 
     std::cout << "Hello World" << std::endl;
     return 0;
